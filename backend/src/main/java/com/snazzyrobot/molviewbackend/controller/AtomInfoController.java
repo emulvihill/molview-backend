ee9df8f24fb10609b663e3f9fb260fb7493934e6
Eric Mulvihill <eric.mulvihill@gmail.com>
2024-11-19 17:37:56 -0500

    Add JWT authentication and CORS configuration
    Integrated JWT for API authentication and configured CORS to permit specific origins. Updated various services and added new components to support token generation and validation.

--- backend/src/main/java/com/snazzyrobot/molviewbackend/controller/AtomInfoController.java
+++ backend/src/main/java/com/snazzyrobot/molviewbackend/controller/AtomInfoController.java
@@ -1,11 +1,12 @@
 package com.snazzyrobot.molviewbackend.controller;
 
 import com.snazzyrobot.molviewbackend.service.AtomInfoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AtomInfoController {
--- backend/src/main/java/com/snazzyrobot/molviewbackend/service/AtomInfoService.java
+++ backend/src/main/java/com/snazzyrobot/molviewbackend/service/AtomInfoService.java
@@ -1,6 +1,10 @@
 package com.snazzyrobot.molviewbackend.service;
 
+import com.snazzyrobot.molviewbackend.controller.AtomInfoController;
 import com.snazzyrobot.molviewbackend.repository.AtomInfoRepository;
+import com.snazzyrobot.molviewbackend.security.JwtUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,13 +12,21 @@ public class AtomInfoService {
 
     private final AtomInfoRepository atomInfoRepository;
     private final com.snazzyrobot.molviewbackend.service.AIService aiService;
+    private final JwtUtil jwtUtil;
 
-    public AtomInfoService(AtomInfoRepository atomInfoRepository, com.snazzyrobot.molviewbackend.service.AIService aiService) {
+    Logger logger = LoggerFactory.getLogger(AtomInfoController.class);
+
+    public AtomInfoService(AtomInfoRepository atomInfoRepository,
+                           AIService aiService,
+                           JwtUtil jwtUtil) {
         this.atomInfoRepository = atomInfoRepository;
         this.aiService = aiService;
+        this.jwtUtil = jwtUtil;
     }
 
     public String getAtomInfo(String pdbFile, int atomId) {
+        String token = jwtUtil.generateToken("testuser");
+        logger.warn(token);
         final String infoResult = aiService.askQuestion(
                 "Give the chemistry properties of atom with id " + atomId + " in the following PDB formatted file:\n" + pdbFile
         );
